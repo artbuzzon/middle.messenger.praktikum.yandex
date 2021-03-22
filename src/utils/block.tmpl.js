5c21c5beb8a5cacce26e2e5e8acbaaf9e66a87d5
Mike <mik.arbuzov@gmail.com>
2021-03-22 18:29:20 +0300

    small refactioring

--- src/utils/block.tmpl.js
+++
@@ -1,8 +0,0 @@
-window.blockTemplate = (function() {
-  return `
-<div class="{{ className }}">
-    <span onClick="{{ handleClick }}">{{text}}</span>
-    <span>{{ user.info.firstName }}</span>
-</div>
-`;
-})();
--- src/utils/templator.js
+++ src/utils/templator.js
@@ -32,3 +32,18 @@ window.Templator = (function () {
   // Можно не только из window брать, но и присвоить экспорту файла
   return Template;
 })();
+
+function get(obj, path, defaultValue) {
+  const keys = path.split('.');
+
+  let result = obj;
+  for (let key of keys) {
+    result = result[key];
+
+    if (result === undefined) {
+      return defaultValue;
+    }
+  }
+
+  return result ?? defaultValue; // "??" — [оператор нулевого слияния](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator) (не поддерживается старыми браузерами, для них нужен полифилл)
+}
