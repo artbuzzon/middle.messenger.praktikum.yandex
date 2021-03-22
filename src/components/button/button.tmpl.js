901e2a3ca927f56b0257c708de6fb4bab2f1c755
Mike <mik.arbuzov@gmail.com>
2021-03-22 18:30:47 +0300

    redundant button component remopved

--- src/components/button/button.tmpl.js
+++
@@ -1,3 +0,0 @@
-export const tmpl = `
-<button class="test">CLick me!</button>
-`;
--- src/components/button/index.js
+++
@@ -1,8 +0,0 @@
-import {tmpl} from './button.tmpl';
-import Hogan from '../../../node_modules/hogan.js/lib/hogan';
-import chatStyle from './button.scss';
-
-const t = Hogan.compile(tmpl);
-const o = t.render({foo: 'Mike!'});
-
-export {o};
