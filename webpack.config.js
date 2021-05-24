cdcff5c01a742716eb74024fe9808fa49d88b1bf
Mike <mik.arbuzov@gmail.com>
2021-05-24 09:52:10 +0300

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/pages/Chat/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'project-name.bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/
+      }
+    ]
+  }
+};
